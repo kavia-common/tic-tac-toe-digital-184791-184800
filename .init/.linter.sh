fb249a2fb629de36dddb81323dbfc0c22eeed37d
Kavia AI <production@kavia.ai>
2025-12-05 19:30:01 +0000

    Checkpoint 1 - cg5bd199dd

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/tic-tac-toe-digital-184791-184800/frontend_tic_tac_toe
+flutter analyze
+
---
+++ frontend_tic_tac_toe/android/app/src/main/kotlin/com/example/frontend_tic_tac_toe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frontend_tic_tac_toe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
